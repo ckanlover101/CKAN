d1268caac05ea8c895d6de0b4ae5ffd412166e0d
Dwayne Bent <dbb@dbb.io>
2019-05-25 07:58:32 -0400

    Detect Breaking Ground DLC

--- Core/DLC/BreakingGroundDlcDetector.cs
+++ Core/DLC/BreakingGroundDlcDetector.cs
@@ -1,14 +1,14 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace CKAN.DLC
 {
     /// <summary>
     /// Represents an object that can detect the presence of the official Making History DLC in a KSP installation.
     /// </summary>
-    public sealed class MakingHistoryDlcDetector : StandardDlcDetectorBase
+    public sealed class BreakingGroundDlcDetector : StandardDlcDetectorBase
     {
-        public MakingHistoryDlcDetector()
-            : base("MakingHistory", new Dictionary<string, string>()
+        public BreakingGroundDlcDetector()
+            : base("BreakingGround", new Dictionary<string, string>()
                 {
                     { "1.0", "1.0.0" }
                 }
